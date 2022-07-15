cbdf543f41d1a88982513397f719fbe0177b6ac2
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-07-14 21:41:09 -0400

    my last commit to this repo

---
+++ app/server/python/script1.py
@@ -0,0 +1,6 @@
+
+import sys
+
+print('Hello from python')
+
+sys.stdout.flush()
---
+++ app/server/python/script2.py
@@ -0,0 +1,7 @@
+import sys
+
+print("Output from Python")
+
+print("First name: " + sys.argv[1])
+
+print({"fname": sys.argv[1], "lname": sys.argv[2]})
