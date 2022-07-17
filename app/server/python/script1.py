8c7bf40f0a4bba271b618dde7b9db845cde0d305
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-07-16 20:55:23 -0400

    adding av editing libraries and some functions

--- app/server/python/script1.py
+++ app/server/python/script1.py
@@ -1,6 +1,65 @@
-
+# pip install open-cv ffmpeg moviepy pydub sox
 import sys
+import cv2
+import os
+from matplotlib.transforms import Transform
+import moviepy.editor as mp
+from pydub import AudioSegment
+from pydub.playback import play
+import sox
 
-print('Hello from python')
+vidpath = os.getcwd() + sys.argv[1].split('"')[0]
+audpath = vidpath.split(".")[0] + ".mp3"
+audpath2 = vidpath.split(".")[0] + "_v2.mp3"
+
+# separates audio from the videofile and saves as mp3 with same filename
+
+
+def strip_audio():
+    vid = mp.VideoFileClip(vidpath)
+    print(vid)
+    vid.audio.write_audiofile(audpath)
+
+# plays the video frame by frame (but it goes really fast)
+
+
+def play_video():
+    cap = cv2.VideoCapture(vidpath)
+    ret, frame = cap.read()
+    while(1):
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q') or ret == False:
+            cap.release()
+            cv2.destroyAllWindows()
+            break
+        cv2.imshow('frame', frame)
+    # release the video capture object
+    cap.release()
+    # Closes all the windows currently opened.
+    cv2.destroyAllWindows()
+
+
+def remix_audio():
+    strip_audio()
+    sox.file_info.validate_input_file(audpath)
+    # create transformer
+    tfm = sox.Transformer()
+    # trim the audio between 5 and 10.5 seconds.
+    # tfm.trim(5, 10.5)
+    # apply compression
+    tfm.compand()
+    # apply a fade in and fade out
+    # tfm.fade(fade_in_len=1.0, fade_out_len=0.5)
+	# apply bandpass filter
+    tfm.bas(gain_db=20)
+    # create an output file.
+    tfm.build_file(input_filepath=audpath, output_filepath=audpath2)
+    # see the applied effects
+    tfm.effects_log
+
+
+remix_audio()
+
 
 sys.stdout.flush()
--- app/server/routes/python.js
+++ app/server/routes/python.js
@@ -2,41 +2,20 @@ const express = require('express')
 const { spawn } = require('child_process');
 const Router = express.Router();
 
-Router.get('/script1', (req, res) => {
+Router.post('/sendFile', (req, res) => {
 	let data1;
 	// spawn new child process to call the python script
-	const python = spawn('python', ['python/script1.py']);
+	const python = spawn('python', ['python/script1.py', req.query.filepath], { stdio: 'inherit' });
 	// collect data from script
-	python.stdout.on('data', function (data) {
+	python.on('data', (data) => {
 		console.log('Pipe data from python script ...');
 		data1 = data.toString();
 	});
-	// in case of error, handle it
-	python.stderr.on('data', (data) => {
-		console.log(data.toString());
-	});
 	// in close event we are sure that stream from child process is closed
 	python.on('close', (code) => {
-		console.log(`child process close all stdio with code ${code}`);
+		console.log(`child process close all stdio with code ${code} -----------------------------------------------`);
 		// send data to browser
 		res.send(data1)
-	});
-})
-
-Router.get('/script2/:fname/:lname', (req, res) => {
-	let data2;
-	// spawn new child process to call the python script
-	const python = spawn('python3', ['python/script2.py', req.params.fname, req.params.lname]);
-	// collect data from script
-	python.stdout.on('data', function (data) {
-		console.log('Pipe data from python script ...');
-		data2 = data;
-	});
-	// in close event we are sure that stream from child process is closed
-	python.on('close', (code) => {
-		console.log(`child process close all stdio with code ${code}`);
-		// send data to browser
-		res.send(data2)
 	});
 })
 
