6167e9d8adb85d0321a64f14ab7d608bc9749f75
rushil-shah87 <54685822+rushilshah87@users.noreply.github.com>
2022-07-18 21:45:42 -0400

    pushing again

--- app/server/routes/python.js
+++ app/server/routes/python.js
@@ -5,19 +5,19 @@ const Router = express.Router();
 const dashes = "----------------------------"
 
 Router.post('/sendFile', (req, res) => {
-	let data1;
+	let data;
 	// spawn new child process to call the python script
 	const python = spawn('python3', ['python/script1.py', req.query.filepath], { stdio: 'inherit' });
 	// collect data from script
-	python.on('data', (data) => {
+	python.on('data', (output) => {
 		console.log('Pipe data from python script ...');
-		data1 = data.toString();
+		data = output.toString();
 	});
 	// in close event we are sure that stream from child process is closed
 	python.on('close', (code) => {
 		console.log(`${dashes} python process shutting down all stdio - code: ${code} ${dashes}`);
 		// send data to browser
-		res.send(data1)
+		console.log(data1)
 	});
 })
 
